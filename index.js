e34e37c97b19b67636df47d1b2ec7b0c6974326c
pandiji <skm808478@gmail.com>
2023-05-16 03:53:23 +0530

    first

---
+++ index.js
@@ -0,0 +1,123 @@
+var query=document.querySelector('.chat-input input');
+var btn=document.querySelector('.btn-submit');
+var chatbtn=document.querySelector('.chat-btn')
+var faq=false;
+document.querySelector('.FAQBTN').addEventListener("click",()=>{
+                const ele= document.createElement('div')
+                ele.classList.add('msg-output')
+                ele.innerHTML='<label>Frequently Asked Queries:</label><br><label>11.What is the purpose of your website?</label><label>12.What types of exams does your website cover?</label><label>13.How can I access the study materials on your website?</label><label>14.do you have crash courses?</label><label>15.do you have any personal mentorship program</label><label>16.how can i contact you?</label><label>17.Back to Main menu</label>'
+                document.querySelector('.chat-body').appendChild(ele)
+})
+chatbtn.addEventListener('click',()=>{
+    document.querySelector('.chat').style.opacity="1"
+})
+document.querySelector(".close").addEventListener("click",()=>{
+    document.querySelector('.chat').style.opacity="0"
+})
+btn.addEventListener('click',()=>{
+    
+    const ele= document.createElement('div')
+    ele.textContent=query.value;
+    ele.classList.add('msg-input')
+    document.querySelector('.chat-body').appendChild(ele)
+    handleQuery(query.value);
+    query.value=""
+})
+function handleQuery(input)
+{
+    const ele= document.createElement('div')
+    if(parseInt(input)>=1 && parseInt(input)<=8)
+    {
+        switch(parseInt(input))
+        {
+            case 1:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/'
+                break;
+            case 2:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/courses'
+                break;
+            case 3:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/selections'
+                break;
+            case 4:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/blog'
+                break;    
+            case 5:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/materials'
+                break;
+            case 6:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com/teams'
+                break;
+            case 7:
+            ele.textContent="Redirecting you to the page.....";
+            ele.classList.add('msg-output')
+            document.querySelector('.chat-body').appendChild(ele)
+                window.location.href='https://www.sciastra.com//contact'
+                break;
+            case 8:
+                ele.classList.add('msg-output')
+                ele.innerHTML='<label>Frequently Asked Queries:</label><br><label>11.What is the purpose of your website?</label><label>12.What types of exams does your website cover?</label><label>13.How can I access the study materials on your website?</label><label>14.do you have crash courses?</label><label>15.do you have any personal mentorship program</label><label>16.how can i contact you?</label><label>17.Back to Main menu</label>'
+                document.querySelector('.chat-body').appendChild(ele)
+                break;         
+        }
+    }
+    else if(parseInt(input)>=11 && parseInt(input)<=17)
+    {
+        const ele= document.createElement('div')
+        switch(parseInt(input))
+        {
+            case 11:ele.classList.add('msg-output')
+            ele.innerHTML='<p>Our website is designed to help students prepare for various competitive exams<br>by providing study materials, practice tests, and other resources.</p>'
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 12:ele.classList.add('msg-output')
+            ele.innerHTML='<p>We cover exams like<br>IISC<br>IAT<br>NEST<br>ISI/CMI<br>IIT-JAM<br>IACS<br>CUET<br>ICAR</p>'
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 13:ele.classList.add('msg-output')
+            ele.innerHTML='<p>You can access the study materials on our website by registering as a user and logging in.<br> Once you are logged in, you can access the study materials from the materials section of the website.</p>'
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 14:ele.classList.add('msg-output')
+            ele.innerHTML="<p>Yes we do have crash courses.<br>For further info: <a href='https://www.sciastra.com/courses'>Click Here</a></p>"
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 15:ele.classList.add('msg-output')
+            ele.innerHTML="<p>Yes we do have personal mentorship course.<br>You can talk to our experinced mentors on video call<br>For further info: <a href='https://www.sciastra.com/courses'>Click Here</a></p>"
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 16:ele.classList.add('msg-output')
+            ele.innerHTML="<p>Our Contact Info:<br>Address:SciAstra Education Pvt Ltd, Bhubaneswar, Odisha<br>Mail:<a href='mailto:support@sciastra.com'>support@sciastra.com</a><br>For further info: <a href='https://www.sciastra.com/courses'>Click Here</a></p>"
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+            case 17:ele.classList.add('msg-output')
+            ele.innerHTML="<a href='https://www.sciastra.com/'>1.Home</a><a href='https://www.sciastra.com/courses'>2.Courses</a><a href='https://www.sciastra.com/selections'>3.Selections</a><a href='https://www.sciastra.com/blog'>4.Blog</a><a href='https://www.sciastra.com/materials'>5.Materials</a><a href='https://www.sciastra.com/teams'>6.Teams</a><a href='https://www.sciastra.com//contact'>7.Contact Us</a><a href='#' class='FAQBTN'>8.FAQs</a>"
+            document.querySelector('.chat-body').appendChild(ele)
+            break;
+        }
+    }
+    else{
+        const ele= document.createElement('div')
+        ele.textContent="Please,Enter correct option no.";
+        ele.classList.add('msg-output')
+        document.querySelector('.chat-body').appendChild(ele)
+    }
+}
---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="chat.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="test"></div>
+    <div class="chat">
+        <div class="close"><div class="close1"></div></div>
+        <div class="chat-head"><label for="chat-head">AstraHelper</label></div>
+        <div class="chat-body">
+        <div class="msg-output">
+            Hi, How can i help You!
+        </div>
+        <div class="msg-output">
+            Please click on an option or enter correct option no.
+        </div>
+        <div class="msg-output">
+            <a href="https://www.sciastra.com/">1.Home</a>
+            <a href="https://www.sciastra.com/courses">2.Courses</a>
+            <a href="https://www.sciastra.com/selections">3.Selections</a>
+            <a href="https://www.sciastra.com/blog">4.Blog</a>
+            <a href="https://www.sciastra.com/materials">5.Materials</a>
+            <a href="https://www.sciastra.com/teams">6.Teams</a>
+            <a href="https://www.sciastra.com//contact">7.Contact Us</a>
+            <a href="#" class="FAQBTN">8.FAQs</a>
+        </div>
+        </div>
+        <div class="chat-input"><input type="text" placeholder="Enter your query?"><button class="btn-submit">Submit</button></div>
+    </div>
+    <img class="chat-btn" src="./asset/chat2.png" alt="">
+   
+
+<script src="index.js"></script>
+</body>
+</html>
